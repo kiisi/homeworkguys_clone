87fe54dedf8b139fd0f6c005311bd3400102bb41
Kiisi Felix Destiny <98293558+Kiisi-Felix-Destiny@users.noreply.github.com>
2022-07-21 04:24:27 +0100

    first commit

--- src/components/Main/Testimony/Testimoney.js
+++ src/components/Main/Testimony/Testimoney.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Testimony.css'
+import cloud0 from '../../Images/Cloud0.png'
+import cloud1 from '../../Images/Cloud1.png' 
 
 const Testimony = () => {
   return (
-    <section className="max-wrapper section-wrapper">
-
+    <section className="max-wrapper testimony-wrapper">
+        
     </section>
   )
 }

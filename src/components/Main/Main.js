ba6a65b70f29518d7caf5cf9c539a91cd4294fc5
Kiisi Felix Destiny <98293558+Kiisi-Felix-Destiny@users.noreply.github.com>
2022-07-25 00:48:03 +0100

    Done

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import Chats from './Chats/Chats'
 import Header from './Header/Header'
 import Request from './Request/Request'
-import Testimony from './Testimony/Testimony'
 
 const Main = () => {
   return (
     <main className='max-wrapper'>
         <Header/>
         <Request/>
-        <Testimony/>
-        <Chats/>
     </main>
   )
 }

76147c6a05e4a1a011fd6d3e5c190e8f60589880
Kiisi Felix Destiny <98293558+Kiisi-Felix-Destiny@users.noreply.github.com>
2022-07-25 15:53:38 +0100

    Done

--- src/components/Main/Chats/Chats.js
+++ src/components/Main/Chats/Chats.js
@@ -1,15 +1,46 @@
 import React from 'react'
 import './Chats.css'
-import flower from '../../Images/aF19.png' 
+import c1 from '../../Images/Satisfiedclient1.jpg'
+import c2 from '../../Images/Satisfiedclient2.jpg'
+import c3 from '../../Images/Satisfiedclient3.jpg'
+import c4 from '../../Images/Satisfiedclient7.jpg'
+import c5 from '../../Images/Screenshot_Instagram.jpg'
 
 const Chats = () => {
   return (
     <section className="chats-wrapper">
-      <div className="chats-flower-container">
-        
-      </div>
+      
       <div className="chats-container">
-            
+        <div className="chats-container-img">
+          <img src={c1} alt="img"/>
+        </div>
+        <div className="chats-container-img">
+          <img src={c2} alt="img"/>
+        </div>
+        <div className="chats-container-img">
+          <img src={c3} alt="img"/>
+        </div>
+        <div className="chats-container-img">
+          <img src={c4} alt="img"/>
+        </div>
+        <div className="chats-container-img">
+          <img src={c5} alt="img"/>
+        </div>
+        <div className="chats-container-img">
+          <img src={c1} alt="img"/>
+        </div>
+        <div className="chats-container-img">
+          <img src={c2} alt="img"/>
+        </div>
+        <div className="chats-container-img">
+          <img src={c3} alt="img"/>
+        </div>
+        <div className="chats-container-img">
+          <img src={c4} alt="img"/>
+        </div>
+        <div className="chats-container-img">
+          <img src={c5} alt="img"/>
+        </div>
       </div>
 
     </section>

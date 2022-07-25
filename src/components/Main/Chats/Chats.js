34079bb2e2620100431425c4057780f169910e94
Kiisi Felix Destiny <98293558+Kiisi-Felix-Destiny@users.noreply.github.com>
2022-07-25 19:13:06 +0100

    Done

--- src/components/Main/Chats/Chats.js
+++ src/components/Main/Chats/Chats.js
@@ -5,41 +5,88 @@ import c2 from '../../Images/Satisfiedclient2.jpg'
 import c3 from '../../Images/Satisfiedclient3.jpg'
 import c4 from '../../Images/Satisfiedclient7.jpg'
 import c5 from '../../Images/Screenshot_Instagram.jpg'
+import flower from '../../Images/flower.png'
 
 const Chats = () => {
   return (
     <section className="chats-wrapper">
-      
+      <div className="chats-flower-container">
+        <div className="flower flower-1">
+          <img src={flower} alt="flower" />
+        </div>
+        <div className="flower flower-2">
+          <img src={flower} alt="flower" />
+        </div>
+        <div className="flower flower-3">
+          <img src={flower} alt="flower" />
+        </div>
+        <div className="flower flower-4">
+          <img src={flower} alt="flower" />
+        </div>
+        <div className="flower flower-5">
+          <img src={flower} alt="flower" />
+        </div>
+      </div>
       <div className="chats-container">
         <div className="chats-container-img">
-          <img src={c1} alt="img"/>
+          <img src={c1} alt="img" />
         </div>
         <div className="chats-container-img">
-          <img src={c2} alt="img"/>
+          <img src={c2} alt="img" />
         </div>
         <div className="chats-container-img">
-          <img src={c3} alt="img"/>
+          <img src={c3} alt="img" />
         </div>
         <div className="chats-container-img">
-          <img src={c4} alt="img"/>
+          <img src={c4} alt="img" />
         </div>
         <div className="chats-container-img">
-          <img src={c5} alt="img"/>
+          <img src={c5} alt="img" />
         </div>
         <div className="chats-container-img">
-          <img src={c1} alt="img"/>
+          <img src={c1} alt="img" />
         </div>
         <div className="chats-container-img">
-          <img src={c2} alt="img"/>
+          <img src={c2} alt="img" />
         </div>
         <div className="chats-container-img">
-          <img src={c3} alt="img"/>
+          <img src={c3} alt="img" />
         </div>
         <div className="chats-container-img">
-          <img src={c4} alt="img"/>
+          <img src={c4} alt="img" />
         </div>
         <div className="chats-container-img">
-          <img src={c5} alt="img"/>
+          <img src={c5} alt="img" />
+        </div>
+        <div className="chats-container-img">
+          <img src={c1} alt="img" />
+        </div>
+        <div className="chats-container-img">
+          <img src={c2} alt="img" />
+        </div>
+        <div className="chats-container-img">
+          <img src={c3} alt="img" />
+        </div>
+        <div className="chats-container-img">
+          <img src={c4} alt="img" />
+        </div>
+        <div className="chats-container-img">
+          <img src={c5} alt="img" />
+        </div>
+        <div className="chats-container-img">
+          <img src={c1} alt="img" />
+        </div>
+        <div className="chats-container-img">
+          <img src={c2} alt="img" />
+        </div>
+        <div className="chats-container-img">
+          <img src={c3} alt="img" />
+        </div>
+        <div className="chats-container-img">
+          <img src={c4} alt="img" />
+        </div>
+        <div className="chats-container-img">
+          <img src={c5} alt="img" />
         </div>
       </div>
 

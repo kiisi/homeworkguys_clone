35c268e54d95f5d9ee5a1d8ea4173db9dbdf192d
Kiisi Felix Destiny <98293558+Kiisi-Felix-Destiny@users.noreply.github.com>
2022-07-25 14:29:15 +0100

    Done

--- src/components/Main/Chats/Chats.js
+++ src/components/Main/Chats/Chats.js
@@ -5,12 +5,12 @@ import flower from '../../Images/aF19.png'
 const Chats = () => {
   return (
     <section className="chats-wrapper">
-        <div className="chats-container">
-            <img src={flower} alt="flower" className="flower flower-1"/>
-            <img src={flower} alt="flower" className="flower flower-2"/>
-            <img src={flower} alt="flower" className="flower flower-3"/>
-            <img src={flower} alt="flower" className="flower flower-4"/>
-        </div>
+      <div className="chats-flower-container">
+        
+      </div>
+      <div className="chats-container">
+            
+      </div>
 
     </section>
   )
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,14 +2,15 @@ import React from 'react'
 import Header from './Header/Header'
 import Request from './Request/Request'
 import Testimony from './Testimony/Testimony'
+import Chats from './Chats/Chats'
 
 const Main = () => {
   return (
     <main className='max-wrapper'>
         <Header/>
         <Request/>
-        <Testimony/>{/*
-        <Chats/> */}
+        <Testimony/>
+        <Chats/>
     </main>
   )
 }
